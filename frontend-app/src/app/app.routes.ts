511ab6325a1844206903a14d7d4472997ea5a775
jose.dantas@jazzhr.com <jose.dantas@jazzhr.com>
2025-07-09 15:24:57 -0300

    Fix routes

--- frontend-app/src/app/app.routes.ts
+++ frontend-app/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { CursoComponent } from './curso/curso.component';
 import { SemestreComponent } from './semestre/semestre.component';
 import { DisciplinaComponent } from './disciplina/disciplina.component';
+import { GradecurricularComponent } from './gradecurricular/gradecurricular.component';
 
 export const routes: Routes = [
   { path: 'curso', component: CursoComponent },
   { path: 'semestre', component: SemestreComponent },
   { path: 'disciplina', component: DisciplinaComponent },
+  { path: 'gradecurricular', component: GradecurricularComponent },
   { path: '', redirectTo: '/curso', pathMatch: 'full' },
 ];

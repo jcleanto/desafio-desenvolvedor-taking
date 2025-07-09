b75413a77a26c324302efeecf231019099b02d39
jose.dantas@jazzhr.com <jose.dantas@jazzhr.com>
2025-07-09 20:33:55 -0300

    Add comments

--- backend-app/src/main/java/org/taking/service/CursoSemestreDisciplinaService.java
+++ backend-app/src/main/java/org/taking/service/CursoSemestreDisciplinaService.java
@@ -29,8 +29,9 @@ public class CursoSemestreDisciplinaService {
 
   @Transactional
   public void create(CursoSemestreDisciplinaDTO cursoSemestreDisciplinaDTO) throws InvalidAttributesException {
-    Validate.notNull(cursoSemestreDisciplinaDTO, "CursoSemestreDisciplina não pode ser nulo");
+    Validate.notNull(cursoSemestreDisciplinaDTO, "CursoSemestreDisciplinaDTO não pode ser nulo");
 
+    // cria a chave primária composta, utilizando os ids de cada entidade (Curso, Semestre e Disciplina), que estão populados no DTO
     CursoSemestreDisciplinaKey cursoSemestreDisciplinaKey = new CursoSemestreDisciplinaKey(cursoSemestreDisciplinaDTO.getCurso().getId(), cursoSemestreDisciplinaDTO.getSemestre().getId(), cursoSemestreDisciplinaDTO.getDisciplina().getId());
     CursoSemestreDisciplina cursoSemestreDisciplina = new CursoSemestreDisciplina();
     cursoSemestreDisciplina.setCursoSemestreDisciplinaKey(cursoSemestreDisciplinaKey);
@@ -40,6 +41,7 @@ public class CursoSemestreDisciplinaService {
 
   @Transactional
   public void delete(CursoSemestreDisciplinaDTO cursoSemestreDisciplinaDTO) {
+    // realiza uma busca pela chave primária composta
     PanacheQuery<CursoSemestreDisciplina> query = cursoSemestreDisciplinaRepository.find("curso_id = :cursoId and semestre_id = :semestreId and disciplina_id = :disciplinaId", Parameters.with("cursoId", cursoSemestreDisciplinaDTO.getCurso().getId()).and("semestreId", cursoSemestreDisciplinaDTO.getSemestre().getId()).and("disciplinaId", cursoSemestreDisciplinaDTO.getDisciplina().getId()));
     Optional<CursoSemestreDisciplina> cursoSemestreDisciplina = query.singleResultOptional();
     Validate.notNull(cursoSemestreDisciplina, "CursoSemestreDisciplina não pode ser nulo");

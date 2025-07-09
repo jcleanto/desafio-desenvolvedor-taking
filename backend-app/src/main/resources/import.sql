5b8c91c584d95ecf10c20bdc1eddd749fd7a659c
jose.dantas@jazzhr.com <jose.dantas@jazzhr.com>
2025-07-09 15:28:38 -0300

    Update import.sql

--- backend-app/src/main/resources/import.sql
+++ backend-app/src/main/resources/import.sql
@@ -3,3 +3,25 @@ INSERT INTO curso(id, name) VALUES (nextval('hibernate_sequence'), 'Arquitetura'
 INSERT INTO curso(id, name) VALUES (nextval('hibernate_sequence'), 'Ciência da Computação');
 INSERT INTO curso(id, name) VALUES (nextval('hibernate_sequence'), 'Direito');
 INSERT INTO curso(id, name) VALUES (nextval('hibernate_sequence'), 'Economia');
+
+INSERT INTO semestre(id, name) VALUES (nextval('hibernate_sequence'), '1º Semestre');
+INSERT INTO semestre(id, name) VALUES (nextval('hibernate_sequence'), '2º Semestre');
+INSERT INTO semestre(id, name) VALUES (nextval('hibernate_sequence'), '3º Semestre');
+INSERT INTO semestre(id, name) VALUES (nextval('hibernate_sequence'), '4º Semestre');
+INSERT INTO semestre(id, name) VALUES (nextval('hibernate_sequence'), '5º Semestre');
+INSERT INTO semestre(id, name) VALUES (nextval('hibernate_sequence'), '6º Semestre');
+INSERT INTO semestre(id, name) VALUES (nextval('hibernate_sequence'), '7º Semestre');
+INSERT INTO semestre(id, name) VALUES (nextval('hibernate_sequence'), '8º Semestre');
+INSERT INTO semestre(id, name) VALUES (nextval('hibernate_sequence'), '9º Semestre');
+INSERT INTO semestre(id, name) VALUES (nextval('hibernate_sequence'), '10º Semestre');
+
+INSERT INTO disciplina(id, name) VALUES (nextval('hibernate_sequence'), 'Disciplina 1');
+INSERT INTO disciplina(id, name) VALUES (nextval('hibernate_sequence'), 'Disciplina 2');
+INSERT INTO disciplina(id, name) VALUES (nextval('hibernate_sequence'), 'Disciplina 3');
+INSERT INTO disciplina(id, name) VALUES (nextval('hibernate_sequence'), 'Disciplina 4');
+INSERT INTO disciplina(id, name) VALUES (nextval('hibernate_sequence'), 'Disciplina 5');
+INSERT INTO disciplina(id, name) VALUES (nextval('hibernate_sequence'), 'Disciplina 6');
+INSERT INTO disciplina(id, name) VALUES (nextval('hibernate_sequence'), 'Disciplina 7');
+INSERT INTO disciplina(id, name) VALUES (nextval('hibernate_sequence'), 'Disciplina 8');
+INSERT INTO disciplina(id, name) VALUES (nextval('hibernate_sequence'), 'Disciplina 9');
+INSERT INTO disciplina(id, name) VALUES (nextval('hibernate_sequence'), 'Disciplina 10');
